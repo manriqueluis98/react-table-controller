bed3eedd73e1f63898623df311cef8cbf3d39eec
manriqueluis98 <manriqueleo98@gmail.com>
2024-02-15 16:08:54 -0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+import ContainerFluid from "@/components/ui/ContainerFluid";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center my-4">
+      <ContainerFluid>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+          temporibus dignissimos perspiciatis fugit unde porro in repudiandae
+          tenetur? Id incidunt rerum voluptates sunt, architecto adipisci
+          maiores recusandae et veritatis quibusdam corporis iusto eveniet
+          facere culpa voluptate. Assumenda distinctio accusantium et inventore
+          quis tempora! Deserunt, accusantium. Quas pariatur incidunt laborum
+          provident consectetur eaque dolorem quod, doloremque ducimus
+          blanditiis excepturi quibusdam enim quaerat maiores vel debitis autem
+          vero nobis ipsum beatae est nostrum voluptatibus corporis deleniti.
+          Commodi consequuntur aliquid cumque asperiores mollitia voluptatum
+          amet veritatis temporibus architecto voluptate quod nesciunt quibusdam
+          debitis repudiandae voluptas rem ipsum illo, hic pariatur labore
+          deserunt eligendi necessitatibus fugiat dolorum? Qui reprehenderit
+          aliquam nostrum sunt error reiciendis adipisci, dolor cum labore id.
+          Ullam quo consequatur beatae vero illum culpa, suscipit cumque earum
+          architecto sapiente unde animi. Deserunt eligendi tenetur accusamus
+          soluta sunt quos nam cumque nihil impedit.
+        </p>
+      </ContainerFluid>
+    </main>
+  );
+}
